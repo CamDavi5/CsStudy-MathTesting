db2f0e6ccd0537a3e7541ad9409694300d017cdf
CamDavi5 <camrdavis.biz@gmail.com>
2022-05-30 17:16:13 -0500

    Add project files.

---
+++ MathTesting/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Lets do Math!");
+Console.WriteLine("Enter the first number:");
+int numOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the first number:");
+int numTwo = Convert.ToInt32(Console.ReadLine());
+
+int sum = numOne + numTwo;
+int difference = numOne - numTwo;
+int product = numOne * numTwo;
+int remainder = numOne / numTwo;
+
+Console.WriteLine("This is the sum: "+sum);
+Console.WriteLine("This is the difference: "+difference);
+Console.WriteLine("This is the product: "+product);
+Console.WriteLine("This is the remainder: "+remainder);
